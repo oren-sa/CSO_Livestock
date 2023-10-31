270290308b1c18d1154a7f27e14a0ff75a9e60cb
Alex Orenstein <alex.merkovic@gmail.com>
2023-10-31 13:59:32 -0400

    Update ndvi_seasonal_calc_portable.R

--- Kenya/Seasonal_ndvi/ndvi_seasonal_calc_portable.R
+++ Kenya/Seasonal_ndvi/ndvi_seasonal_calc_portable.R
@@ -21,7 +21,7 @@ read_season_crop<- function (x){
   cropped_raster<-crop_aoi(ndvi_raster)
   return(cropped_raster)
 }
-year_list<- c("2007","2008","2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023")
+year_list<- c("2008","2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023")
 #write season 1 max rasters
 for (year in year_list){
   pattern <- paste0(year, '(031|032|033|041|042|043|051|052|053).tif')
